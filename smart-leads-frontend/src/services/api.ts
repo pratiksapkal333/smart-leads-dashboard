5c0a169d83e2d053461009495be017d025fcddb0
pratiksapkal333 <pratiksapkal333@gmail.com>
2026-05-19 19:50:35 +0530

    Added api link of backend to deploy project

--- smart-leads-frontend/src/services/api.ts
+++ smart-leads-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://smart-leads-dashboard-backend-1m9c.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
