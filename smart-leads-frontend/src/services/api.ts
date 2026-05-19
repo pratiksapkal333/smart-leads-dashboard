2ff8349b6371c5fc2fcceb1111a1e7500adc544c
pratiksapkal333 <pratiksapkal333@gmail.com>
2026-05-19 21:08:20 +0530

    api error solved

--- smart-leads-frontend/src/services/api.ts
+++ smart-leads-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://smart-leads-dashboard-backend-1m9c.onrender.com",
+    baseURL: "https://smart-leads-dashboard-backend-1m9c.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
